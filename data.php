8f0a2029f5e457be7d6b250903eb6b2442e1b14b
farhanbello <145187892+farhanbello@users.noreply.github.com>
2023-09-16 17:54:44 +0200

    Add files via upload

--- data.php
+++ data.php
@@ -1,11 +1,11 @@
 <?php
-    // Récupérez la valeur du champ "Tache" du formulaire
+    
     $tache = $_POST["Taches"];
     $importance = $_POST["Importance"];
     $echeance = $_POST["Echeance"];
     include 'database.php';
 
-    // Requête SQL pour insérer la tâche dans la base de données
+    
     $requete = "INSERT INTO tache (Taches,Importance,Echeance) VALUES ('$tache','$importance','$echeance')";
     
     $result=mysqli_query($connexion,$requete);
